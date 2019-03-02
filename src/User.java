0b4e53942c96a56fc91ca963b869edfeb4810afc
kjctar <2412864341@qq.com>
2019-03-02 10:57:45 +0800

    hello world and git study

--- src/User.java
+++ src/User.java
@@ -1,9 +1,13 @@
 public class User {
 
     private final String name;
+    private String password;
 
 
-    public User(String name){
+    public User(String name,String password){
         this.name = name;
+
+
+        this.password = password;
     }
 }
